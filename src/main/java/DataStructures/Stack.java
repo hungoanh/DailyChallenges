cf2657a6cc500452608af34230819edb8d816efe
Hung Le <the.bakonator@outlook.com>
2018-01-20 11:54:55 -0700

    Adding some data structure classes.

--- src/main/java/DataStructures/Stack.java
+++ src/main/java/DataStructures/Stack.java
@@ -3,5 +3,40 @@ package main.java.DataStructures;
 /**
  * Created by hungle on 1/20/18.
  */
-public class Stack {
+public class Stack<T> {
+
+    private class Node {
+        T item;
+        Node next;
+    }
+
+    private Node first = null;
+
+    public void push(T item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+
+    public T pop() {
+        Node oldFirst = first;
+        first = first.next;
+        return oldFirst.item;
+    }
+
+    public int size() {
+        Node tmp = first;
+        int count = 0;
+        while (tmp != null) {
+            count++;
+            tmp = tmp.next;
+        }
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
 }
--- src/test/DataStructures/StackOfStringsTests.java
+++ src/test/DataStructures/StackOfStringsTests.java
@@ -2,6 +2,7 @@ package test.DataStructures;
 
 import static org.junit.Assert.*;
 
+import main.java.DataStructures.Stack;
 import main.java.DataStructures.StackOfStrings;
 import main.java.DataStructures.StackOfStrings2;
 import org.junit.Test;
@@ -70,6 +71,46 @@ public class StackOfStringsTests {
     @Test
     public void testStackOfStringsIsEmptySuccess2() {
         StackOfStrings2 stackOfStrings = new StackOfStrings2(10);
+        stackOfStrings.push("One");
+        stackOfStrings.push("Two");
+        assertEquals(false, stackOfStrings.isEmpty());
+        stackOfStrings.pop();
+        stackOfStrings.pop();
+        assertEquals(true, stackOfStrings.isEmpty());
+    }
+
+    @Test
+    public void testStackGenericPushPopSuccess() {
+        Stack<String> stackOfStrings = new Stack<>();
+        stackOfStrings.push("One");
+        stackOfStrings.push("Two");
+        assertEquals("Two", stackOfStrings.pop());
+        assertEquals("One", stackOfStrings.pop());
+
+        Stack<Integer> stackOfIntegers = new Stack<>();
+        stackOfIntegers.push(12);
+        stackOfIntegers.push(12/2);
+        assertEquals((Integer) 6, stackOfIntegers.pop());
+        assertEquals((Integer) 12, stackOfIntegers.pop());
+    }
+
+    @Test
+    public void testStackGenericSizeSuccess() {
+        Stack<String> stackOfStrings = new Stack<>();
+        stackOfStrings.push("One");
+        stackOfStrings.push("Two");
+        assertEquals(2, stackOfStrings.size());
+        // Pop one size--
+        stackOfStrings.pop();
+        assertEquals(1, stackOfStrings.size());
+        // Pop again size--
+        stackOfStrings.pop();
+        assertEquals(0, stackOfStrings.size());
+    }
+
+    @Test
+    public void testStackGenericIsEmptySuccess() {
+        Stack<String> stackOfStrings = new Stack<>();
         stackOfStrings.push("One");
         stackOfStrings.push("Two");
         assertEquals(false, stackOfStrings.isEmpty());
